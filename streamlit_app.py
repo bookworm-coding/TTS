eaa655eeaafa68a7bb8d58a7c3ea6c06001359a5
bookworm-coding <bookwormkkm00@gmail.com>
2023-11-02 18:52:39 +0900

    자동 언어 감지 추가 및 aiogtts에서 gtts로 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import asyncio
 from io import BytesIO
-from aiogtts import aiogTTS
-from streamlit_extras.no_default_selectbox import selectbox
+from gtts import gTTS
+from langdetect import detect
+from langdetect import DetectorFactory
+DetectorFactory.seed = 0
 
 st.set_page_config(
     page_title="TTS",
@@ -11,32 +12,17 @@ st.set_page_config(
 )
 
 
-async def TTS():
-    gtts = aiogTTS()
-    if lang == "한국어":
-        l = 'ko'
-    elif lang == "영어":
-        l = 'en'
-    else:
-        st.info("언어를 선택해 주세요.")
-        return
+def tts():
+    gtts = gTTS(text=text, lang=detect(text))
     b = BytesIO()
-    await gtts.write_to_fp(text=text, lang=l, fp=b)
+    gtts.write_to_fp(b)
     st.audio(b)
     st.download_button(label="다운로드", data=b, file_name=text+".mp3")
+    return
 
 
 st.title("TTS")
 text = st.text_area(label="TTS 오디오를 생성할 텍스트를 입력하세요.")
-lang = selectbox(
-    "언어를 선택해 주세요.",
-    ["한국어", "영어"],
-    no_selection_label="언어를 선택해 주세요."
-)
-
-
-def tts():
-    asyncio.run(TTS())
 
 
 st.button("TTS 오디오 생성", on_click=tts)
